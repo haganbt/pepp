364f048290140bc10ba3d7169b0d8a63a780dd87
Ben <benhagan@gmail.com>
2015-12-29 14:49:09 -0800

    Single task tests

---
+++ test/integration/single.task.freqDist.test.js
@@ -0,0 +1,37 @@
+"use strict";
+process.env.NODE_ENV = 'test';
+
+const _ = require('underscore');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const queue = require('../../lib/queue');
+const taskProcessor = require('../../lib/taskProcessor');
+
+const config = {
+    "freqDist": [
+        {
+            "target": "fb.author.region",
+            "threshold": 2
+        }
+    ]
+};
+
+
+describe("Single task tests", function(){
+
+    it('freqDist', function() {
+
+        let configTasks = taskProcessor.loadConfigTasks(config);
+
+        var value = queue.queueTask(configTasks[0]).then(function(obj) {
+            return obj[1]; // check 2 values are returned
+        });
+
+        return expect(value).to.eventually.to.have.all.keys(["key", "interactions", "unique_authors"]);
+
+    });
+
+});
---
+++ test/integration/single.task.timeSeries.test.js
@@ -0,0 +1,37 @@
+"use strict";
+process.env.NODE_ENV = 'test';
+
+const _ = require('underscore');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const queue = require('../../lib/queue');
+const taskProcessor = require('../../lib/taskProcessor');
+
+const config = {
+    "timeSeries": [
+        {
+            "interval": "day",
+            "span": 1
+        }
+    ]
+};
+
+
+describe("Single task tests", function(){
+
+    it.only('timeSeries', function() {
+
+        let configTasks = taskProcessor.loadConfigTasks(config);
+
+        return queue.queueTask(configTasks[0]).then(function(result){
+
+            expect(result[0]).to.have.all.keys(["key", "interactions", "unique_authors"]);
+            expect(result[0].key).to.be.a('number'); //timestamp
+
+        });
+    });
+
+});

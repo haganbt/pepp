0f656ecfd65afb527439f22f7f16c3e8f0d596e3
Ben <benhagan@gmail.com>
2016-01-01 18:26:58 -0800

    Merged native nested.

--- test/integration/merged.native.nested.test.js
+++ test/integration/merged.native.nested.test.js
@@ -11,63 +11,114 @@ const queue = require('../../lib/queue');
 const taskProcessor = require('../../lib/taskProcessor');
 const taskHelper = require('../../lib/helpers/task');
 
-const config = {
-    "freqDist": [
-        {
-            "merged_native_nested": [
+
+describe.only("Merged Native Nested", function(){
+
+    it('2 level nested', function() {
+
+        let config = {
+            "freqDist": [
                 {
-                    "id":"booboo",
-                    "target": "fb.author.region",
-                    "threshold": 2,
-                    "child": {
-                        "target": "fb.author.age",
-                        "threshold": 2,
-                        "child": {
-                            "target": "fb.author.gender",
-                            "threshold": 2
+                    "merged_native_nested": [
+                        {
+                            "id":"booboo",
+                            "target": "fb.author.region",
+                            "threshold": 2,
+                            "child": {
+                                "target": "fb.author.age",
+                                "threshold": 2
+                            }
+                        },
+                        {
+                            "id": "yogi",
+                            "target": "fb.author.region",
+                            "threshold": 2,
+                            "child": {
+                                "target": "fb.author.age",
+                                "threshold": 2
+                            }
                         }
-                    }
-                },
-                {
-                    "id": "yogi",
-                    "target": "fb.author.region",
-                    "threshold": 2,
-                    "child": {
-                        "target": "fb.author.age",
-                        "threshold": 2,
-                        "child": {
-                            "target": "fb.author.gender",
-                            "threshold": 2
-                        }
-                    }
+                    ]
                 }
             ]
-        }
-    ]
-};
-
-
-describe("Merged Native Nested", function(){
-
-    it.skip('3 level nested', function() {
+        };
 
         let configTasks = taskProcessor.loadConfigTasks(config);
 
+        return Promise.all(configTasks.map(function(each) {
+            return queue.queueTask(each);
+        })).then(function(result){
+
+            result = taskHelper.compact(result);
+
+            expect(result[0].booboo[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            expect(result[0].booboo[1]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+
+            expect(result[0].yogi[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            expect(result[0].yogi[1]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+        });
+    });
 
 
+    it('3 level nested', function() {
 
-        //return
+        let config = {
+            "freqDist": [
+                {
+                    "merged_native_nested": [
+                        {
+                            "id":"booboo",
+                            "target": "fb.author.region",
+                            "threshold": 2,
+                            "child": {
+                                "target": "fb.author.age",
+                                "threshold": 2,
+                                "child": {
+                                    "target": "fb.author.gender",
+                                    "threshold": 2
+                                }
+                            }
+                        },
+                        {
+                            "id": "yogi",
+                            "target": "fb.author.region",
+                            "threshold": 2,
+                            "child": {
+                                "target": "fb.author.age",
+                                "threshold": 2,
+                                "child": {
+                                    "target": "fb.author.gender",
+                                    "threshold": 2
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        };
 
-           // result = taskHelper.compact(result);
+        let configTasks = taskProcessor.loadConfigTasks(config);
 
-            //console.log(JSON.stringify(result, undefined, 4));
+        return Promise.all(configTasks.map(function(each) {
+            return queue.queueTask(each);
+        })).then(function(result){
 
-            //expect(result[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
-            //expect(result[0].child).to.have.all.keys(["analysis_type", "parameters", "results", "redacted"]);
-            //expect(result[0].child.results[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
-            //expect(result[0].child.results[1]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            result = taskHelper.compact(result);
 
-       // });
+            expect(result[0].booboo[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            expect(result[0].booboo[1]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+
+            expect(result[0].yogi[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            expect(result[0].yogi[1]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+
+            expect(result[0].booboo[0].child.results[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            expect(result[0].booboo[1].child.results[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+
+            expect(result[0].yogi[0].child.results[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+            expect(result[0].yogi[1].child.results[0]).to.have.all.keys(["key", "interactions", "unique_authors", "child"]);
+
+        });
+
     });
 
 });

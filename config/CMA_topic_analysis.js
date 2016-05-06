0af0a93612877441de13ba5513f1363602eb7473
Ben <benhagan@gmail.com>
2016-05-06 16:01:19 -0700

    Config file: CONTENT & MEDIA ANALYSIS - Topic Analysis

---
+++ config/CMA_topic_analysis.js
@@ -0,0 +1,247 @@
+/**
+ * CONTENT & MEDIA ANALYSIS
+ * - Topic Analysis
+ *
+ * 1. Define default and baseline index credentials
+ * 2. Populate topic names of interest (CASE SENSITIVE!)
+ *
+ * API CALLS: ~240
+ *
+ */
+
+"use strict";
+
+const moment = require('moment');
+
+const topic_1 = "<TOPIC_1>";
+const topic_2 = "<TOPIC_2>";
+const topic_3 = "<TOPIC_3>";
+const topic_4 = "<TOPIC_4>";
+const topic_5 = "<TOPIC_5>";
+const topic_6 = "<TOPIC_6>";
+const topic_7 = "<TOPIC_7>";
+const topic_8 = "<TOPIC_8>";
+const topic_9 = "<TOPIC_9>";
+const topic_10 = "<TOPIC_10>";
+
+module.exports = {
+    "app": {
+        "format": "csv",
+        "write_to_file": true
+    },
+    "index": {
+        "default": {
+            "id": "<RECORDING_ID>",
+            "auth": {
+                "username": "<USERNNAME>",
+                "api_key": "<API_KEY>"
+            }
+        },
+        "baseline": {
+            "id": "<BASELINE_RECORDING_ID>",
+            "auth": {
+                "username": "<BASELINE_USERNNAME>",
+                "api_key": "<BASELINE_API_KEY>"
+            }
+        }
+    },
+    "start": moment.utc().subtract(13, 'days').unix(),    
+    "analysis": {
+        "freqDist": [
+        /**
+         * Top topics over time
+         */            
+            {
+                "name": "timeSeries_by_topics",
+                "target": "fb.parent.topics.name",
+                "threshold": 50,
+                "then": {
+                    "type": "timeSeries",
+                    "interval": "hour"
+                }
+            },
+        /**
+         * Links by topics
+         */            
+            {
+                "name": "links_by_topics",
+                "target": "fb.parent.topics.name",
+                "threshold": 50,
+                "then": {
+                    "target": "links.url",
+                    "threshold": 50
+                }
+            },
+        /**
+         * Hashtags by topics
+         */
+            {
+                "name": "hashtags_by_topics",
+                "target": "fb.parent.topics.name",
+                "threshold": 50,
+                "then": {
+                    "target": "fb.parent.hashtags",
+                    "threshold": 50
+                }
+            },
+        /**
+         * Topics of topics
+         */
+            {
+                "name": "topics_by_topics",
+                "target": "fb.parent.topics.name",
+                "threshold": 50,
+                "then": {
+                    "target": "fb.parent.topics.name",
+                    "threshold": 20
+                }
+            },
+        /**
+         * Categories and topics
+         */
+            {
+                "name": "topics_by_category",
+                "target": "fb.parent.topics.category",
+                "threshold": 20,
+                "then": {
+                    "target": "fb.parent.topics.name",
+                    "threshold": 20
+                }
+            },
+        /**
+         * Age Gender tornadoes
+         */
+            {
+                "name": "age_gender_by_category",
+                "target": "fb.parent.topics.category",
+                "threshold": 20,
+                "child": {
+                    "target": "fb.author.age",
+                    "threshold": 6,
+                    "child": {
+                        "target": "fb.author.gender",
+                        "threshold": 2
+                    }
+                }
+            },
+        /**
+         * Baseline engagement topics and content
+         */
+            {
+                "engagement_topics_baseline": [
+                    {
+                        "id": "baseline",
+                        "index": "baseline",
+                        "filter": "fb.type != \"story\"",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_1,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_1 + "\" or fb.parent.content any \"" + topic_1 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_2,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_2 + "\" or fb.parent.content any \"" + topic_2 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_3,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_3 + "\" or fb.parent.content any \"" + topic_3 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_4,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_4 + "\" or fb.parent.content any \"" + topic_4 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_5,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_5 + "\" or fb.parent.content any \"" + topic_5 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_6,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_6 + "\" or fb.parent.content any \"" + topic_6 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_7,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_7 + "\" or fb.parent.content any \"" + topic_7 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_8,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_8 + "\" or fb.parent.content any \"" + topic_8 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_9,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_9 + "\" or fb.parent.content any \"" + topic_9 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    },
+                    {
+                        "id": topic_10,
+                        "filter": "fb.type != \"story\" and (fb.parent.content any \"" + topic_10 + "\" or fb.parent.content any \"" + topic_10 + "\")",
+                        "target": "fb.parent.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.parent.author.gender",
+                            "threshold": 2
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};

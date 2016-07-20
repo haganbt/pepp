cb3ec6a9ccee83a5ebfeedfa9de9f3b18e0bc954
Ben Hagan <benhagan@gmail.com>
2016-07-19 18:02:43 -0700

    Config options for 1.4 and generate a default task using them.

--- config/default.js
+++ config/default.js
@@ -6,10 +6,10 @@ module.exports = {
         "write_to_file": false, // true, false
         "max_parallel_tasks": 10, // number of parallel requests
         "log_level": "info", // warn, info, debug, trace
-        "analyze_uri": "https://api.datasift.com/v1.3/pylon/analyze",
         "date_format": "YYYY-MM-DD HH:mm:ss",
-        "resource": "task", // analyze
-        "service": "linkedin", // facebook
-        "api_version": "1.4"
+        "api_base_url": "https://api.datasift.com/",
+        "api_resource": "task", // analyze, task
+        "api_version": "1.4",
+        "service": "linkedin" // facebook, linkdin
     }
 };

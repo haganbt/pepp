0d46384790ddf49f3bb80463e520b78d612e94db
Ben Hagan <benhagan@gmail.com>
2016-07-20 17:14:02 -0700

    Successfully creating new tasks.

--- config/default.js
+++ config/default.js
@@ -5,11 +5,11 @@ module.exports = {
         "format": "json", // json, csv
         "write_to_file": false, // true, false
         "max_parallel_tasks": 10, // number of parallel requests
-        "log_level": "info", // warn, info, debug, trace
+        "log_level": "debug", // warn, info, debug, trace
         "date_format": "YYYY-MM-DD HH:mm:ss",
         "api_base_url": "https://api.datasift.com/",
-        "api_resource": "analyze", // analyze, task
-        "api_version": "1.3",
+        "api_resource": "task", // analyze, task
+        "api_version": "1.4",
         "service": "linkedin" // facebook, linkdin
     }
 };

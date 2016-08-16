cd8cb45ae36eee6b4989ddabe5bfe7cf64457e04
Ben <benhagan@gmail.com>
2016-08-15 21:37:14 -0700

    Add ability to pass in an entire config object to a single run() function.

---
+++ cli.js
@@ -0,0 +1,83 @@
+"use strict";
+process.env.NODE_ENV = process.env.NODE_ENV || "demo";
+
+const _ = require('underscore');
+const figlet = require('figlet');
+const config = require('config');
+
+const taskManager = require('./lib/taskManager');
+const queue = require('./lib/queue');
+const log = require("./lib/helpers/logger");
+const cacheHelper = require('./lib/helpers/cache');
+const format = require('./lib/format');
+const baseline = require('./lib/baseline');
+const file = require('./lib/file');
+const requestFactory = require("./lib/requestFactory").requestFactory;
+
+log.info(figlet.textSync(process.env.NODE_ENV));
+console.log("\n\n");
+
+
+exports.run = function run(configObject={}) {
+
+    const normalizedTasks = taskManager.loadConfigTasks(configObject);
+
+    normalizedTasks.forEach(task => {
+
+        const reqObj = requestFactory(task, configObject.index, configObject.start, configObject.end);
+
+        queue.queueRequest(reqObj, task, configObject.index, configObject.start, configObject.end)
+            .then(response => {
+
+                //handle expected unresolved promises caused by recursion
+                if(response === undefined || _.isEmpty(response)){
+                    return reject();
+                }
+
+                cacheHelper.debugAll();
+
+                return response;
+            })
+            .then(response => {
+
+                if(reqObj.name.includes('baseline')) {
+                    return baseline.gen(response, reqObj);
+                } else {
+                    return format.jsonToCsv(response);
+                }
+
+            })
+            .then(response => {
+                return file.write(reqObj.name, response);
+            })
+            .then(response => {
+
+                if(_.isObject(response)){
+                    log.info(JSON.stringify(response, undefined, 4));
+                }
+
+                if(_.isString(response)){
+                    log.info(response);
+                }
+                console.log("\n");
+
+            })
+            .catch(err => {
+                if(err.response.body){
+                    log.error(err.response.body);
+                } else {
+                    log.error(JSON.stringify(err, undefined, 4));
+                }
+                process.exit(1);
+            });
+    });
+
+};
+
+
+
+
+
+
+
+

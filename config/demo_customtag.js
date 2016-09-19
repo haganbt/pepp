50311aa530c4405395146f2d2fcaa500681dfada
Ben Hagan <benhagan@gmail.com>
2016-09-19 13:31:43 -0700

    Fix tab indentation and remove commented lines.

--- config/demo_customtag.js
+++ config/demo_customtag.js
@@ -2,77 +2,68 @@
 
 module.exports = {
     "app": {
-      "format": "csv",
-      "write_to_file": false,
-      //"log_level": "info", // warn, info, debug, trace
+        "format": "csv",
+        "write_to_file": false
     },
-
     "index": {
-      "default": {
-        "subscription_id": "<SUBSCRIPTION_ID>",
-        "auth": {
-          "username": "<USERNNAME>",
-          "api_key": "<API_KEY>"
+        "default": {
+            "subscription_id": "<SUBSCRIPTION_ID>",
+            "auth": {
+                "username": "<USERNNAME>",
+                "api_key": "<API_KEY>"
+            }
         }
-      }
     },
     "customTags": {
-      "gender": [
-          {
-            "key":"male",
-            "filter": "fb.author.gender == \"male\""
-          },
-          {
-            "key":"female",
-            "filter": "fb.author.gender == \"female\""
-          }
-      ],
-      "age": [
-          {
-            "key":"18-24",
-            "filter": "fb.author.age == \"18-24\""
-          },
-          {
-            "key":"25-34",
-            "filter": "fb.author.age == \"25-34\""
-          }
-      ]
+        "gender": [
+            {
+                "key":"male",
+                "filter": "fb.author.gender == \"male\""
+            },
+            {
+                "key":"female",
+                "filter": "fb.author.gender == \"female\""
+            }
+        ],
+        "age": [
+            {
+                "key":"18-24",
+                "filter": "fb.author.age == \"18-24\""
+            },
+            {
+                "key":"25-34",
+                "filter": "fb.author.age == \"25-34\""
+            }
+        ]
     },
     "analysis": {
-      "freqDist": [
-
-        {
-          "name": "example-fd-task_tag",
-          "custom_tag": "gender",
-          "then": {
-            "custom_tag": "age",
-            "then": {
-              //"baseline":true,              
-              "target": "fb.topics.name",
-              "threshold":2
-          }
-          }
-        },
-        {
-          "name": "example-fd-task",
-                    "target": "fb.author.gender",
-          "threshold": 2,
-          "then": {
-            "target": "fb.author.age",
-            "threshold": 6,
-            "then": {
-            //"baseline":true,
-              "target": "fb.topics.name",
-              "threshold":2
+        "freqDist": [
+            {
+                "name": "example-fd-task_tag",
+                "custom_tag": "gender",
+                "then": {
+                    "custom_tag": "age",
+                    "then": {
+                        "target": "fb.topics.name",
+                        "threshold":2
+                    }
+                }
+            },
+            {
+                "name": "example-fd-task",
+                "target": "fb.author.gender",
+                "threshold": 2,
+                "then": {
+                    "target": "fb.author.age",
+                    "threshold": 6,
+                    "then": {
+                        "target": "fb.topics.name",
+                        "threshold":2
+                    }
+                }
             }
-          }
-        }
-
-      ],
-      "timeSeries": [
-
-        
-
-      ]
+        ],
+        "timeSeries": [
+        ]
     }
 };

7144af97c0dfc2484dabdf80605a6df7c6d6d8a6
benhagan <benhagan@gmail.com>
2016-11-09 09:47:23 -0800

    Current state

--- plugins/example.js
+++ plugins/example.js
@@ -4,6 +4,7 @@ module.exports = function(reqObj) {
     return new Promise(function(resolve, reject) {
 
         console.log("plugin called-----------------------------------");
+        console.log(JSON.stringify(reqObj, undefined, 4));
 
 
 

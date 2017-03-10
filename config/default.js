3cea0705b4526a20e6096dff527c7a861fedf436
benhagan <benhagan@gmail.com>
2017-03-10 09:54:44 -0800

    Set defaults to be more useful for task api.

--- config/default.js
+++ config/default.js
@@ -2,12 +2,12 @@
 
 module.exports = {
     "app": {
-        "format": "json", // json, csv
-        "write_to_file": false, // true, false
-        "max_parallel_tasks": 10, // number of parallel requests
+        "format": "csv", // json, csv
+        "write_to_file": true, // true, false
+        "max_parallel_tasks": 1, // number of parallel requests
         "log_level": "info", // warn, info, debug, trace
         "date_format": "YYYY-MM-DD HH:mm:ss",
-        "api_resource": "analyze", // analyze, task
+        "api_resource": "task", // analyze, task
         "task_uri": "https://api.datasift.com/v1.4/pylon/linkedin/task",
         "analyze_uri": "https://api.datasift.com/v1.3/pylon/analyze"
     }

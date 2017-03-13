a343afb380ae516499c065b2a2ba8ad727f1a145
benhagan <benhagan@gmail.com>
2017-03-13 10:12:22 -0700

    Linting.

--- plugins/baseline.js
+++ plugins/baseline.js
@@ -1,23 +1,22 @@
 "use strict";
 
-module.exports = function(
+module.exports = function (
   defaultResponse,
   n, // normalized response
   configValue,
   log,
   task
 ) {
-  return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
+        log.info("----------------------------------------------------");
+        log.info(JSON.stringify(defaultResponse, undefined, 4));
+        log.info("----------------------------------------------------");
+        log.info(JSON.stringify(n, undefined, 4));
+        log.info("----------------------------------------------------");
+        log.info(JSON.stringify(configValue, undefined, 4));
+        log.info("----------------------------------------------------");
+        log.info(JSON.stringify(task, undefined, 4));
 
-    log.info("----------------------------------------------------");
-    log.info(JSON.stringify(defaultResponse, undefined, 4));
-    log.info("----------------------------------------------------");
-    log.info(JSON.stringify(n, undefined, 4));
-    log.info("----------------------------------------------------");
-    log.info(JSON.stringify(configValue, undefined, 4));
-    log.info("----------------------------------------------------");
-    log.info(JSON.stringify(task, undefined, 4));
-
-    resolve(defaultResponse);
-  });
+        resolve(defaultResponse);
+    });
 };
--- plugins/selfBaseline.js
+++ plugins/selfBaseline.js
@@ -1,8 +1,7 @@
 "use strict";
 
-module.exports = function(defaultResponse, normalizedResponse, configValue, log, task) {
-    return new Promise(function(resolve, reject) {
-
+module.exports = function (defaultResponse, normalizedResponse, configValue, log, task) {
+    return new Promise((resolve, reject) => {
         log.info("----------------------------------------------------");
         log.info(defaultResponse);
         log.info(normalizedResponse);

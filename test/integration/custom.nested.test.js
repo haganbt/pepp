426c400c70f91d55bf90273cb576c153c8f7330c
benhagan <benhagan@gmail.com>
2017-03-27 16:35:39 -0700

    Current state.

--- test/integration/custom.nested.test.js
+++ test/integration/custom.nested.test.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const _ = require("underscore");
 const chai = require("chai");
 const expect = chai.expect;
 const chaiAsPromised = require("chai-as-promised");
@@ -10,10 +9,8 @@ const taskManager = require("../../lib/taskManager");
 const requestFactory = require("../../lib/requestFactory").requestFactory;
 const queue = require("../../lib/queue");
 
-//const taskHelper = require('../../lib/helpers/task');
-
-describe("Custom Nested", function() {
-  it("2 level depth", function() {
+describe("CUSTOM NESTED", function() {
+  it.skip("1 level", async () => {
     const config = {
       freqDist: [
         {
@@ -27,40 +24,64 @@ describe("Custom Nested", function() {
       ]
     };
 
-    const normalizedTasks = taskManager.loadConfigTasks(config);
+    const task = taskManager.loadConfigTasks(config);
+    const reqObj = requestFactory(task[0]);
+    const result = await queue.queueRequest(reqObj, task[0]);
 
-    normalizedTasks.forEach(task => {
+    expect(result).to.be.an("array");
+    expect(result[0]).to.be.an("object");
 
-      const reqObj = requestFactory(task);
+    console.log(JSON.stringify(result, undefined, 4));
 
-      queue
-        .queueRequest(reqObj, task)
-        .then(response => {
+    for (let [key, value] of Object.entries(result[0])) {
 
-          console.log(JSON.stringify(response, undefined, 4));
+      expect(result[0][key]).to.have.all.keys([
+        "interactions",
+        "unique_authors",
+        "male",
+        "female"
+      ]);
+      expect(result[0][key]["male"]).to.have.all.keys([
+        "interactions",
+        "unique_authors"
+      ]);
+      expect(result[0][key]["female"]).to.have.all.keys([
+        "interactions",
+        "unique_authors"
+      ]);
+    }
+
+  });
+
+  it("2 level", async () => {
+    const config = {
+      freqDist: [
+        {
+          target: "li.user.member.country",
+          threshold: 2,
+          then: {
+            target: "li.all.articles.author.member.gender",
+            threshold: 2,
+            then: {
+              target: "li.user.member.employer_industry_sectors",
+              threshold: 2
+            }
+          }
+        }
+      ]
+    };
+
+    const task = taskManager.loadConfigTasks(config);
+    const reqObj = requestFactory(task[0]);
+    const result = await queue.queueRequest(reqObj, task[0]);
+
+    console.log(JSON.stringify(result, undefined, 4));
+
+    expect(result).to.be.an("array");
+    expect(result[0]).to.be.an("object");
 
 
-        });
-    });
+
 
   });
 });
-
-/*
-
-        let configTasks = taskProcessor.loadConfigTasks(config);
-
-        return queue.queueRequest(configTasks[0]).then(function(result){
-
-            expect(result[0]).to.have.all.keys(["male", "female", "unknown"]);
-
-            expect(result[0]["male"]).to.be.an('array');
-            expect(result[0]["male"]).to.have.length(2);
-
-            expect(result[0]["female"]).to.be.an('array');
-            expect(result[0]["female"]).to.have.length(2);
-
-        });
-
-
-*/
